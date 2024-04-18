a3724238665e5c89afa5c12936d224bbaa88c251
남정현 <namjeonghyeon@namjeonghyeon-ui-MacBookAir.local>
2024-04-18 17:24:55 +0900

    upgrade

--- mobick-helper.py
+++ mobick-helper.py
@@ -1,7 +1,7 @@
 import itertools
 from bitcoin import *
 
-def find_valid_private_key(base_key, max_index, indices):
+def find_valid_private_key(base_key, indices):
     base58_characters = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     # 주어진 인덱스에 대해 모든 가능한 Base58 문자를 대입
     for combo in itertools.product(base58_characters, repeat=len(indices)):
@@ -15,16 +15,13 @@ def find_valid_private_key(base_key, max_index, indices):
     return False  # 유효한 키를 찾지 못하면 False 반환
 
 # 초기 설정
-partial_private_key = '프라이빗키'
-initial_indices = [1, 2, 3]
-max_index = len(partial_private_key) - 1
+partial_private_key = '프라이빗키 입력'
+key_length = len(partial_private_key)
+max_index = key_length - 1
 
-# 초기 인덱스 세트에서 시작
-if not find_valid_private_key(partial_private_key, max_index, initial_indices):
-    # 초기 세트가 실패하면 다른 조합 시도
-    for i in range(max(initial_indices) + 1, max_index + 1):
-        new_indices = initial_indices[:-1] + [i]
-        if find_valid_private_key(partial_private_key, max_index, new_indices):
-            break
-    else:
-        print("No valid private key found after trying all combinations.")
+# 모든 조합의 인덱스를 생성
+for combo_indices in itertools.combinations(range(key_length), 3):
+    if find_valid_private_key(partial_private_key, list(combo_indices)):
+        break
+else:
+    print("No valid private key found after trying all combinations.")
